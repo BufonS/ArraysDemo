b56c3b5f7ffd4337d731de3fc5eb6beb0d57cc15
BufonS <Curso Maana@DESKTOP-0LVMFH7>
2024-05-29 12:48:44 +0200

    variante no recomendada para array

--- src/main/java/com/example/ArraysDemo/App.java
+++ src/main/java/com/example/ArraysDemo/App.java
@@ -17,5 +17,15 @@ public class App
     	String[] nombres = {
     			"Tamara","Jessy","Carlos","Adrián","Maria Lopez","Pablo","Maria Jose","Rosa"
     	};
+    	
+    	// Para mostrar por consola los elementos del arrays de nombres
+    	// VARIANTE#1 utilizando una funcion pritnln, mostrar cada elemento del array
+    	System.out.println(nombres[0]);
+    	System.out.println(nombres[1]);
+    	// ... Y, como podran apreciar, la variante #1 NO SIRVE para nada
+    	
+    	// VARIANTE#2 Implica utilizar una setencia de control de flujo para recorder la array y 
+    	// lo veremos en la rama "setenciasdecontroldeflujo"
+    	
     }
 }
