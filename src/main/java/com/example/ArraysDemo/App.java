740fe1244c6aa21b6f4ca014e98a0116da98c753
BufonS <Curso Maana@DESKTOP-0LVMFH7>
2024-05-29 14:46:13 +0200

    sentencia for clasica

--- src/main/java/com/example/ArraysDemo/App.java
+++ src/main/java/com/example/ArraysDemo/App.java
@@ -1,5 +1,7 @@
 package com.example.ArraysDemo;
 
+import java.util.Iterator;
+
 /**
  * Hello world!
  *
@@ -26,6 +28,41 @@ public class App
     	
     	// VARIANTE#2 Implica utilizar una setencia de control de flujo para recorder la array y 
     	// lo veremos en la rama "setenciasdecontroldeflujo"
+    	
+    	// PRIMERO Con setencia for de toda la vida, o clasica 
+    	for(int i = 0; i < nombres.length; i++ ){
+    		System.out.println(nombres[i]);
+    	}	
+    	/*Ejercicio1 Solamente mostrar los nombres del array de nombres que tienen
+    	 * mas de 4 caracteres*/
+    	for(int i = 0; i < nombres.length; i++) {
+    		if (nombres[i].length() > 4) {
+    			
+    		System.out.println(nombres[i]);
+    		}
+    	}
+    	/*Que diferencia hay en utilizar el operador de autoincremento, o 
+    	 * autodecremnto antes o despues
+    	 * 
+    	 * PAra solucionar este problema, clasico d cuando empieza con un lenguaje de programacion 
+    	 * utilizaremos JSHELL, que es como un Shell, que es un shell de python
+    	 * que antes java no tenia
+    	 * 
+    	 * RTA. El operador de autoinccremento o autodecremento si esta solo en una sentencia,
+    	 * da igual que vaya antes o despues, es decir, i++; o ++i; SON EXACTAMENTE LO MISMO,
+    	 * sin embarg, i + ++j no es lo mismo que i + j++
+    	 * 
+    	 * NOTA MUY IMPORTANTE!!!! Cuando se utiliza el for clasico?
+    	 * Cuando nos interesa el indice. 
+    	 *  EJERCICIO#2 Por ejemplo, si queremos recorrer del array de numeros
+    	 *  y solamente mostrar los elementos que son indices */
+    	for(int i= 0; i < numeros.length; i++) {
+    	if (i % 2 == 0)
+    		System.out.println("el elemneto de indice " + i + ", es par");
+    	}
+    	
+    	//SEGUNDO con un for mejorado, en ocasiones MAL llamado for each (por cada)
+    	// y digo mal llamado porque la setencia forEach existe, como veran posteriormente
     	
     }
 }
