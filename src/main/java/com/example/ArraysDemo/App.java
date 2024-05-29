216283459fc842d67803fe20573078e5f9f359d3
BufonS <Curso Maana@DESKTOP-0LVMFH7>
2024-05-29 11:16:01 +0200

    mejor varainte de Array

--- src/main/java/com/example/ArraysDemo/App.java
+++ src/main/java/com/example/ArraysDemo/App.java
@@ -8,14 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
+    	// Variante dos 
     	//Como se declara una Array ?
-    	int[] numeros; // Celaramos que numeros es una array de valores enteros
-    	// Como agregar valores al array de numeros 
-        // PRIMERAMENTE a que reservar memoria oara los elemontos de la array
-    	numeros = new int[10]; 
-    	numeros[0] = 1;
-    	numeros[1] = 2;
-    	numeros[2]= 3;
-    	// asi hasta el indice 9 el 0(cuenta) al 9 hay 10 numeros
+    	int[] numeros = {
+    		0,1,2,3,4,5,6,7,8,9,10,11,12,13,14	
+    	};
+    	// Un array de nombre seria 
+    	String[] nombres = {
+    			"Tamara","Jessy","Carlos","Adrián","Maria Lopez","Pablo","Maria Jose","Rosa"
+    	};
     }
 }
